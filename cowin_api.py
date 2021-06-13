6426fe9e1d12807836207a4f82b3e9510cfa1b67
Aaditya Tile <adi01tile@gmail.com>
2021-06-13 14:19:59 +0530

    testing on pythonanywhere

--- cowin_api.py
+++ cowin_api.py
@@ -1,3 +1,9 @@
+# TODO: make it more dynamic and versatile for other users like chatid, ability to select their location.
+# 1003584231 chat id
+# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/getUpdates
+# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/sendMessage?chat_id=1003584231&text=hello
+# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/sendMessage?chat_id=1003584231&text=```trying\n%20to%20make%20\n%20newline```&parse_mode=MarkdownV2
+
 import requests
 from datetime import datetime
 from playsound import playsound
@@ -6,18 +12,22 @@ import json
 import os
 
 current_date = datetime.today().strftime('%d-%m-%Y')
+current_time = datetime.now().strftime("%H:%M:%S")
 district_id = 363  #374, state_id = 21
 notification = "gamey_notification.wav"
 telegram_key = os.environ['cowin_telegram_bot']
+end_log = "- at {} https://www.cowin.gov.in/home".format(current_time)
 
 results = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(district_id, current_date))
 results = results.json()
 
+def telegram_bot(msg):
+	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id=1003584231&text={}".format(telegram_key, msg))
+
 def check_slots():
-	noti_flag = 0
 	for result in results['sessions']:
 		msg = {}
-		if result["min_age_limit"] == 18 and result["fee_type"] == 'Free':
+		if result["min_age_limit"] == 45 and result["fee_type"] == 'Free':
 			if result["available_capacity"] == 0:
 				msg['Name'] = result['name']
 				msg['Address'] = result['address']
@@ -31,17 +41,12 @@ def check_slots():
 				msg = msg.replace("}", "")
 				msg = msg.replace('"', "")
 				msg = msg.replace(", ", "\n")
-				print(msg) #msg will be a formatted string now
+				telegram_bot(msg) #msg will be a formatted string now
+				print(msg) 
 				print('-'*20)
-				telegram_bot(msg)
-				noti_flag = 1
-	return noti_flag
-
-def telegram_bot(msg):
-	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id=1003584231&text={}".format(telegram_key, msg))
-
+	playsound(notification)
+	telegram_bot(end_log)
 
 while True:
-	if check_slots():
-		playsound(notification)
+	check_slots()
 	sleep(15)
