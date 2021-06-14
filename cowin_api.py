eeb97b7e1aa0f74dbe00889fdebdf40ed5cd5591
Aaditya Tile <adi01tile@gmail.com>
2021-06-14 23:09:26 +0530

    local version ready and usable

--- cowin_api.py
+++ cowin_api.py
@@ -1,36 +1,32 @@
 # TODO: make it more dynamic and versatile for other users like chatid, ability to select their location.
-# TODO: check for different dates
-# 1003584231 chat id
-# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/getUpdates
-# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/sendMessage?chat_id=1003584231&text=hello
-# https://api.telegram.org/bot1753315617:AAEOTzux5XHZANJX7qIIDa7oKlFhSXo_oJs/sendMessage?chat_id=1003584231&text=```trying\n%20to%20make%20\n%20newline```&parse_mode=MarkdownV2
+# TODO: dynamically get chat id
+# https://api.telegram.org/bot<bot_token>/getUpdates
+# https://api.telegram.org/bot<bot_token>/sendMessage?chat_id=1003584231&text=hello
 
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 from playsound import playsound
 from time import sleep
 import json
 import os
 
 current_date = datetime.today().strftime('%d-%m-%Y')
-current_time = datetime.now().strftime("%H:%M:%S")
-district_id = 363  #374, 395, state_id = 21
+next_day1 = (datetime.today() + timedelta(days=1)).strftime('%d-%m-%Y')
+next_day2 = (datetime.today() + timedelta(days=2)).strftime('%d-%m-%Y')
+days_li = [current_date, next_day1, next_day2]
+district_id = 374  #374, 363, 395, state_id = 21
 notification = "gamey_notification.wav"
-telegram_key = os.environ['cowin_telegram_bot']
-end_log = "- at {} https://www.cowin.gov.in/home".format(current_time)
-
-results = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(district_id, current_date))
-results = results.json()
+telegram_key = os.environ['COWIN_TELEGRAM_BOT']
 
 def telegram_bot(msg):
 	requests.get("https://api.telegram.org/bot{}/sendMessage?chat_id=1003584231&text={}".format(telegram_key, msg))
 
-def check_slots():
-	noti_flag = 0
+def check_slots(results):
+	cnt = 0
 	for result in results["sessions"]:
 		msg = {}
-		if result["min_age_limit"] == 45 and result["fee_type"] == 'Free':
-			if result["available_capacity"] == 0:
+		if result["min_age_limit"] == 18 and result["fee_type"] == 'Free':
+			if result["available_capacity_dose1"] != 0:
 				msg['Name'] = result['name']
 				msg['Address'] = result['address']
 				msg['Pincode'] = result['pincode']
@@ -45,12 +41,18 @@ def check_slots():
 				msg = msg.replace(", ", "\n")
 				telegram_bot(msg) #msg will be a formatted string now
 				print(msg) 
-				print('-'*20)
-				noti_flag = 1
-	return noti_flag
+				print('-'*25)
+				cnt += 1
+	return cnt
 
 while True:
-	if check_slots():
-		playsound(notification)
-		telegram_bot(end_log)
-	sleep(15)
+	for day in days_li:
+		results = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(district_id, day))
+		results = results.json()
+		cnt = check_slots(results)
+		if cnt:
+			playsound(notification)
+			end_log = "- {} results at {} https://www.cowin.gov.in/home".format(cnt, day)
+			print(end_log)
+			telegram_bot(end_log)
+	sleep(12)
