9c7df337d84bde8660807410c2ce94f656aa8495
Aaditya Tile <adi01tile@gmail.com>
2021-06-22 13:00:13 +0530

    finishing touches

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-# https://api.telegram.org/bot<bot_token>/getUpdates
-# https://stackoverflow.com/a/38388851/8533259
-
 import requests
 from datetime import datetime, timedelta
 from playsound import playsound
@@ -9,8 +6,12 @@ import json
 import os
  
 telegram_key = os.environ['COWIN_TELEGRAM_BOT']  #your telegram bot token goes here
-telegram_chat_id = os.environ['COWIN_TELEGRAM_CHAT_ID']  #chat id for telegram bot
+telegram_chat_id = os.environ['COWIN_TELEGRAM_CHAT_ID']  #chat id for telegram bot goes here
 notification = "gamey_notification.wav"
+
+headers = {
+	'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+}
 
 # reading district_id
 with open("location.txt", "r") as file:
@@ -56,7 +57,7 @@ if __name__ == '__main__':
 	while True:
 		days_li = days_to_fetch(3) #will check for these many days
 		for day in days_li:
-			results = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(district_id, day))
+			results = requests.get('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}'.format(district_id, day), headers=headers)
 			results = results.json()
 			cnt = check_slots(results)
 			if cnt:
